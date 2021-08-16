0c1a24ba998f535fcaf8bf1bb6e7390f9a6740ed
LongN22 <lnguyen7274@gmail.com>
2021-08-16 16:26:24 -0500

    Initalized table

--- internship-applications-tracker/src/App.js
+++ internship-applications-tracker/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Job from './Job';
-import { Button, FormControl, Input, InputLabel } from '@material-ui/core';
+import { Button, FormControl, Input, InputLabel, TableCell, TableContainer, Paper, Table, TableHead, TableRow } from '@material-ui/core';
 import './App.css';
 import db from './Firebase'
 import Firebase from 'firebase'
@@ -17,14 +17,19 @@ function App() {
 
   // Array of jobs (short-term storage. Resets after refresh)
   const [jobList, setJobList] = useState([]);
-  const [input, setInput] = useState('');
-  console.log(input);
+  const [position, setPosition] = useState('');
+  const [description, setDescription] = useState('');
+  const [pay, setPay] = useState('');
+  const [company, setCompany] = useState('');
+
+
+  console.log(position);
 
   // Load database from Firebase
   useEffect(() => {
     db.collection('jobList').orderBy('timestamp').onSnapshot(snapshot => {
       console.log(snapshot.docs.map(doc => doc.data().position));
-      setJobList(snapshot.docs.map(doc => ({id: doc.id, position: doc.data().position, status: doc.data().status})));
+      setJobList(snapshot.docs.map(doc => ({id: doc.id, position: doc.data().position, status: doc.data().status, description: doc.data().description, pay: doc.data().pay, company: doc.data().company})));
     })
   }, []);
 
@@ -32,24 +37,49 @@ function App() {
     console.log('added');
     // Push input into jobList
     db.collection('jobList').add({
-      position: input,
+      position: position,
+      description: description,
+      pay: pay,
+      company, company,
       timestamp: Firebase.firestore.FieldValue.serverTimestamp(),
       status: 'Applied'
     })
     // Clears input box after submission
-    setInput('');
+    setPosition('');
+    setDescription('');
+    setPay('');
+    setCompany('');
   }
+
+
   
   return (
     <div className="App">
       <h1>Internship Applications Tracker</h1>
       <FormControl>
         <InputLabel>Position</InputLabel>
-        <Input value={input} onChange={event => setInput(event.target.value)}/>
+        <Input value={position} onChange={event => setPosition(event.target.value)}/>
       </FormControl>
-      <Button disabled={!input} onClick={addJob} variant="contained" color="primary">
+
+      <FormControl>
+      <InputLabel>Company</InputLabel>
+      <Input value={company} onChange={event => setCompany(event.target.value)}/>
+      </FormControl>
+
+      <FormControl>
+      <InputLabel>Description</InputLabel>
+      <Input value={description} onChange={event => setDescription(event.target.value)}/>
+      </FormControl>
+
+      <FormControl>
+      <InputLabel>Pay</InputLabel>
+      <Input value={pay} onChange={event => setPay(event.target.value)}/>
+      </FormControl>
+
+      <Button disabled={!position} onClick={addJob} variant="contained" color="primary">
       Add
       </Button>
+
       
       <ul>
         {jobList.map(jobList => (
--- internship-applications-tracker/src/Job.js
+++ internship-applications-tracker/src/Job.js
@@ -1,21 +1,49 @@
 import React from 'react'
-import { List, ListItem, ListItemText, Button } from '@material-ui/core';
+import { List, ListItem, ListItemText, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, DataGrid } from '@material-ui/core';
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 import db from './Firebase';
 
 // rfce: shortcut for creation
 
+/* <List >
+            <ListItem divider={true} dense={true}>
+                <ListItemText primary={props.job.position} secondary={props.job.company}/>
+                <ListItemText primary={props.job.description}/>
+
+                <ListItemText primary={props.job.pay}/>
+                <DeleteForeverIcon onClick={event => db.collection('jobList').doc(props.job.id).delete()}/>
+
+            </ListItem>
+            
+        </List> */
+
+
+
+
 function Job(props) {
 
     return (
-        <List>
-            <ListItem>
-                <ListItemText primary={props.job.position}/>
-                <ListItemText primary={props.job.status}/>
-            </ListItem>
-            <Button onClick={event => db}>change</Button>
-            <DeleteForeverIcon onClick={event => db.collection('jobList').doc(props.job.id).delete()}/>
-        </List>
+    <TableContainer component={Paper}>
+        <Table aria-label="simple table">
+            <TableHead>
+                <TableRow>
+                    <TableCell>Position</TableCell>
+                    <TableCell>Description</TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                <TableRow key={props.job.position}>
+                    <TableCell style={{ maxWidth: 0}}>
+                        {props.job.position}
+                    </TableCell>
+                    <TableCell style={{ maxWidth: 0}}>
+                        {props.job.description}
+                    </TableCell>
+                    <DeleteForeverIcon onClick={event => db.collection('jobList').doc(props.job.id).delete()}/>
+                </TableRow>
+            </TableBody>
+        </Table>
+    </TableContainer>
     )
 }
 
